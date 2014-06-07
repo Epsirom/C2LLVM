313b627c4c48bf6f1b2e9cc0e36139f40a23403d
wangxiaoyou11 <wxyyxw09090@sina.com>
2014-06-08 00:45:29 +0800

    temp commit

--- tests/test-main.js
+++ tests/test-main.js
@@ -7,4 +7,4 @@ console.log("配置成功了耶~");
 var C2LLVM  = require('../index');
 
 var compiler = new C2LLVM('SimpleCv4');
-console.log(compiler.compile("tests/data/inputC1"));
+console.log(compiler.compile("tests/data/arraylist.c"));
